0143077470a1b0e69c85ba1a82018812d41d4a3c
udaykumar-m <udaykumardevil3830@gmail.com>
2023-06-06 16:46:52 +0530

    added a style spinner and delay show and hide

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
     $('.joke').hide();
+    $('.spinner').hide();
+    $('.spinner').show(300);
+    var currentHour = new Date().getHours();
+    var greeting;
+    if (currentHour >= 5 && currentHour < 12) {
+      greeting = 'Good Morning';
+    } else if (currentHour >= 12 && currentHour < 18) {
+      greeting = 'Good Afternoon';
+    } else if (currentHour >= 18 && currentHour < 22) {
+      greeting = 'Good Evening';
+    } else {
+      greeting = 'Good Night';
+    }
+    $('.greeting').text(greeting);
 });
