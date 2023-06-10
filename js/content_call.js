e0267ee0f1948f62b8e8dfcc57e9619afcd1db98
udaykumar-m <udaykumardevil3830@gmail.com>
2023-06-10 18:21:47 +0530

    added code to get the input for openAI prompt

--- js/content_call.js
+++ js/content_call.js
@@ -11,18 +11,29 @@ $('.text').hide();
 let value
 
 const getResult = async (param) =>{
-    let query = `simple Meaning of word ${param}`
+    $('.text').hide();
+    $('.spinner').show('100');
+    let query
+    if (title === 'Meaning and Usage') 
+        query = `Define the word "${param}" concisely`
+    else if (title === 'Instagram Caption') 
+        query = `Create a captivating Instagram caption on : "${param}" concisely`
+    else if (title === 'Generate a Tweet') 
+        query = `Compose a concise tweet about "${param}"`
+
     console.log(query)
     let generatedText = await makeAPICall(apiKey, "https://api.openai.com/v1/completions", query) ;
     console.log(generatedText)
-    document.getElementById('apiResult').innerHTML = generatedText ;
-    query = `simple usage of word ${param}`
-    generatedText = await makeAPICall(apiKey, "https://api.openai.com/v1/completions", query) ;
-    console.log(generatedText)
-    generatedText = `${param} usage`+ generatedText.replace(/<br>/g, "");
-    document.getElementById('usage').innerHTML = generatedText ;
+    document.getElementById('meaning').innerHTML = generatedText ;
+    if (title === 'Meaning and Usage') {
+        query = `concise sentence using the word ${param}`
+        generatedText = await makeAPICall(apiKey, "https://api.openai.com/v1/completions", query) ;
+        console.log(generatedText)
+        generatedText = `<b>${param}</b> usage : `+ generatedText.replace(/<br>/g, "");
+        document.getElementById('usage').innerHTML = generatedText ;
+    }
     $('.spinner').hide('100');
-    $('.text').show(400);
+    $('.text').show('200');
 }
 
 
@@ -31,3 +42,19 @@ if (localStorage.getItem("selectedValue")) {
     console.log('selected value :'+value)
     getResult(value)
 }
+else{
+    $('.spinner').hide('100');
+    $('.text').show(400);
+}
+
+$("#find").click(function() {
+    var inputValue = $("#search").val();
+    getResult(inputValue)
+});
+
+$("#search").keypress(function(event) {
+    if (event.which === 13){
+        var inputValue = $("#search").val();
+        getResult(inputValue)
+    }
+});
